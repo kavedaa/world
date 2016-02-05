420d4f9e7ebb42727adb2310b865ec2a836b2686
Knut Arne Vedaa <post@vedaadata.no>
2016-02-05 13:53:50 +0100

    formatting on Currency

--- src/main/scala/no/vedaadata/world/db/model/Currency.scala
+++ src/main/scala/no/vedaadata/world/db/model/Currency.scala
@@ -8,4 +8,7 @@ case class Currency(
   alphaCode: String,
   numericCode: Option[Int],
   defaultName: String,
-  minorUnits: Option[Int])
+  minorUnits: Option[Int]) {
+  
+  def codeAndName = s"$alphaCode - $defaultName"
+}
